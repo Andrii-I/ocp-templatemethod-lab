9707b34440ae903f3ea6782afb88c4a36fcd5d54
Andrii-I <ieroshenkoa@gmail.com>
2020-05-22 21:06:06 -0700

    added EspressoCoffee class, added espresso brewing to CoffeeClient

--- src/lab/assignment/afterrerefactoring/clientcode/CoffeeClient.java
+++ src/lab/assignment/afterrerefactoring/clientcode/CoffeeClient.java
@@ -3,12 +3,9 @@ package lab.assignment.afterrerefactoring.clientcode;
 import lab.assignment.afterrerefactoring.CoffeeMaker;
 import lab.assignment.afterrerefactoring.MochaCoffee;
 import lab.assignment.afterrerefactoring.CapuccinoCoffee;
-
-
+import lab.assignment.afterrerefactoring.AmericanoCoffee;
 
 public class CoffeeClient {
-
-
     public static void main(String[] args) {
         CoffeeMaker starbuzz = new MochaCoffee();
         starbuzz.prepareCoffee();
@@ -16,6 +13,11 @@ public class CoffeeClient {
         System.out.println("********************");
 
         starbuzz = new CapuccinoCoffee();
+        starbuzz.prepareCoffee();
+
+        System.out.println("********************");
+
+        starbuzz = new AmericanoCoffee();
         starbuzz.prepareCoffee();
 
     }
